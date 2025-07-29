ed221cc7656a8ed765e42849e16ffc54ded963d3
Kumar Prince <39616644+thekrprince@users.noreply.github.com>
2025-07-29 16:59:18 +0000

    NC 7th done

---
+++ NeetCode-250/Arrays & Hashing/7. remove-element.js
@@ -0,0 +1,17 @@
+// LC 27 - Remove Element
+
+function removeElement(nums, val) {
+  let k = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] !== val) {
+      nums[k] = nums[i];
+      k++;
+    }
+  }
+  console.log(nums);
+  return k;
+}
+
+console.log(removeElement([3, 2, 2, 3], 3));
+console.log(removeElement([0, 1, 2, 2, 3, 0, 4, 2], 2));
