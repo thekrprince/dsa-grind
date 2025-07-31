cf063bb0c5e76ab9873a75d50061870bbb116458
Kumar Prince <39616644+thekrprince@users.noreply.github.com>
2025-07-31 17:04:05 +0000

    LC 12th Problem

---
+++ NeetCode-250/1. Arrays & Hashing/12. sort-colors.js
@@ -0,0 +1,25 @@
+// LC 75 - Sort Colors
+
+function sortColors(nums) {
+  let low = 0;
+  let mid = 0;
+  let high = nums.length - 1;
+
+  while (mid <= high) {
+    if (nums[mid] === 0) {
+      [nums[low], nums[mid]] = [nums[mid], nums[low]];
+      low++;
+      mid++;
+    } else if (nums[mid] === 1) {
+      mid++;
+    } else {
+      [nums[mid], nums[high]] = [nums[high], nums[mid]];
+      high--;
+    }
+  }
+
+  console.log(nums);
+}
+
+sortColors([2, 0, 2, 1, 1, 0]);
+sortColors([2, 0, 1]);
