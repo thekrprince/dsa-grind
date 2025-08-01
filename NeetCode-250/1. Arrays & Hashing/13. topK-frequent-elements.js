479e536820e08709dd21956cb2f72c886fe02bd2
princekr96 <princekr700@gmail.com>
2025-08-01 06:53:51 +0530

    NC 13th Problem

--- NeetCode-250/1. Arrays & Hashing/13. topK-frequent-elements.js
+++ NeetCode-250/1. Arrays & Hashing/13. topK-frequent-elements.js
@@ -1,1 +1,21 @@
 // LC 347 - Top K Frequent Elements
+function topKFrequent(nums, k) {
+    const numsObj = {};
+
+    for (let num of nums) {
+        numsObj[num] = (numsObj[num] || 0) + 1;
+    }
+
+    const sortedObj = Object.entries(numsObj).sort((a, b) => b[1] - a[1]);
+
+    const topKEle = [];
+
+    for (let i = 0; i < k; i++) {
+        topKEle.push(parseInt(sortedObj[i][0]));
+    }
+
+    return topKEle;
+}
+
+console.log(topKFrequent([1, 1, 1, 2, 3, 3], 2));
+console.log(topKFrequent([1], 1));
