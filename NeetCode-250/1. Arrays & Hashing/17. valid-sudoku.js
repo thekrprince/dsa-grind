9a1bb20942f4aaeb5c791122c1ade74aa1a0fe03
princekr96 <princekr700@gmail.com>
2025-08-02 09:37:44 +0530

    LC 17th problem

---
+++ NeetCode-250/1. Arrays & Hashing/17. valid-sudoku.js
@@ -0,0 +1,51 @@
+
+function isValidSudoku(board) {
+    let rows = Array.from({ length: 9 }, () => new Set());
+    let cols = Array.from({ length: 9 }, () => new Set());
+    let boxes = Array.from({ length: 9 }, () => new Set());
+
+    for (let r = 0; r < rows.length; r++) {
+        for (let c = 0; c < cols.length; c++) {
+            let value = board[r][c];
+
+            if (value === ".") {
+                continue;
+            }
+
+            let boxIndex = Math.floor(r / 3) * 3 + Math.floor(c / 3);
+
+            if (rows[r].has(value) || cols[c].has(value) || boxes[boxIndex].has(value)) {
+                return false;
+            }
+
+            rows[r].add(value);
+            cols[c].add(value);
+            boxes[boxIndex].add(value);
+        }
+    }
+
+    return true;
+}
+
+let board1 = [["5", "3", ".", ".", "7", ".", ".", ".", "."]
+    , ["6", ".", ".", "1", "9", "5", ".", ".", "."]
+    , [".", "9", "8", ".", ".", ".", ".", "6", "."]
+    , ["8", ".", ".", ".", "6", ".", ".", ".", "3"]
+    , ["4", ".", ".", "8", ".", "3", ".", ".", "1"]
+    , ["7", ".", ".", ".", "2", ".", ".", ".", "6"]
+    , [".", "6", ".", ".", ".", ".", "2", "8", "."]
+    , [".", ".", ".", "4", "1", "9", ".", ".", "5"]
+    , [".", ".", ".", ".", "8", ".", ".", "7", "9"]];
+
+let board2 = [["8", "3", ".", ".", "7", ".", ".", ".", "."]
+    , ["6", ".", ".", "1", "9", "5", ".", ".", "."]
+    , [".", "9", "8", ".", ".", ".", ".", "6", "."]
+    , ["8", ".", ".", ".", "6", ".", ".", ".", "3"]
+    , ["4", ".", ".", "8", ".", "3", ".", ".", "1"]
+    , ["7", ".", ".", ".", "2", ".", ".", ".", "6"]
+    , [".", "6", ".", ".", ".", ".", "2", "8", "."]
+    , [".", ".", ".", "4", "1", "9", ".", ".", "5"]
+    , [".", ".", ".", ".", "8", ".", ".", "7", "9"]];
+
+console.log(isValidSudoku(board1));
+console.log(isValidSudoku(board2));
