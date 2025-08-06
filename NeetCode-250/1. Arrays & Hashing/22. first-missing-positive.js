1288720a2e32cbff37631533320df84122e5d18b
princekr96 <princekr700@gmail.com>
2025-08-06 06:53:41 +0530

    NC 22nd Problem

---
+++ NeetCode-250/1. Arrays & Hashing/22. first-missing-positive.js
@@ -0,0 +1,25 @@
+// LC 41 - First Missing Positive
+
+function firstMissingPositive(nums) {
+    let n = nums.length;
+
+    for (let i = 0; i < n; i++) {
+        while (nums[i] > 0 && nums[i] <= n && nums[nums[i] - 1] !== nums[i]) {
+            let temp = nums[i];
+            nums[i] = nums[temp - 1];
+            nums[temp - 1] = temp;
+        }
+    }
+
+    for (let i = 0; i < n; i++) {
+        if (nums[i] !== i + 1) {
+            return i + 1;
+        }
+    }
+
+    return n + 1;
+}
+
+console.log(firstMissingPositive([1, 2, 0]));
+console.log(firstMissingPositive([3, 4, -1, 1]));
+console.log(firstMissingPositive([7, 8, 9, 11, 12]));
