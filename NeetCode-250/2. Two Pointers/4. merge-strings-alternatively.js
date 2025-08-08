3b87622455c3ca8509e34d7dc090a885a9602a8e
princekr96 <princekr700@gmail.com>
2025-08-08 07:16:47 +0530

    NC 26th Problem

--- NeetCode-250/2. Two Pointers/4. merge-strings-alternatively.js
+++ NeetCode-250/2. Two Pointers/4. merge-strings-alternatively.js
@@ -20,4 +20,4 @@ function mergeAlternately(word1, word2) {
 
 console.log(mergeAlternately("abc", "pqr")); // apbqcr
 console.log(mergeAlternately("ab", "pqrs")); // apbqrs
-console.log(mergeAlternately("abcd", "pq")); // apbqcd
+console.log(mergeAlternately("abcd", "pq"));  // apbqcd
