130e8583069696565e02c79e77d3e22a741484c2
princekr96 <princekr700@gmail.com>
2025-08-15 12:26:35 +0530

    NC - 35th Prob

--- NeetCode-250/3. Sliding Window/1. contains-duplicate-2.js
+++ NeetCode-250/3. Sliding Window/1. contains-duplicate-2.js
@@ -1,7 +1,19 @@
-// LC 219 - Contains Duplicate 2
+// LC 219 - Contains Duplicate 2 - https://leetcode.com/problems/contains-duplicate-ii/description/
 
 function containsDuplicate(nums, k) {
     let set = new Set();
+    let i = 0;
+
+    for (let j = 1; j < nums.length; j++) {
+        if (set.has(nums[i])) return true;
+        set.add(nums[i]);
+
+        if (set.size > k) {
+            set.delete(nums[i]);
+        }
+    }
+
+    return false;
 }
 
 console.log(containsDuplicate([1, 2, 3, 1], 3));
