51bf98d54bcd4bc50ed17f3cb860124e128ece26
princekr96 <princekr700@gmail.com>
2025-08-15 10:44:18 +0530

    updated the edge case

--- NeetCode-250/2. Two Pointers/10. rotate-array.js
+++ NeetCode-250/2. Two Pointers/10. rotate-array.js
@@ -1,7 +1,7 @@
 // LC 189 - Rotate Array - https://leetcode.com/problems/rotate-array/description/
 
 function rotateArray(nums, k) {
-    let i = 0;
+    k = k % nums.length;
 
     function reverse(i, j) {
         while (i < j) {
@@ -11,12 +11,15 @@ function rotateArray(nums, k) {
         }
     }
 
-    reverse(i, nums.length - 1);
-    reverse(i, k - 1);
+    reverse(0, nums.length - 1);
+    reverse(0, k - 1);
     reverse(k, nums.length - 1);
 
     console.log(nums);
 }
 
-rotateArray([1, 2, 3, 4, 5, 6, 7], 3);
-rotateArray([-1, -100, 3, 99], 2);
+rotateArray([1, 2, 3, 4, 5, 6, 7], 3); // [5, 6, 7, 1, 2, 3, 4]
+rotateArray([-1, -100, 3, 99], 2); // [ 3, 99, -1, -100 ]
+
+// Below example is the reason why do we have to do k%nums.length
+rotateArray([1, 2, 3, 4, 5], 7); // [ 4, 5, 1, 2, 3 ]
