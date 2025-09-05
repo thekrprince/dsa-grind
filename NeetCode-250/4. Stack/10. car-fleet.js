51ef00afa6d47c40fd352cc45549ff110d8142da
princekr96 <princekr700@gmail.com>
2025-09-05 21:53:40 +0530

    car fleet

--- NeetCode-250/4. Stack/10. car-fleet.js
+++ NeetCode-250/4. Stack/10. car-fleet.js
@@ -1,1 +1,25 @@
 // LC 853. Car Fleet - https://leetcode.com/problems/car-fleet/description/
+
+function carFleet(target, position, speed) {
+    let cars = [];
+
+    for (let i = 0; i < position.length; i++) {
+        cars.push([position[i], (target - position[i]) / speed[i]]);
+    }
+
+    cars.sort((a, b) => b[0] - a[0]);
+
+    let stack = [];
+
+    for (let [pos, time] of cars) {
+        if (stack.length === 0 || time > stack[stack.length - 1]) {
+            stack.push(time);
+        }
+    }
+
+    return stack.length;
+}
+
+console.log(carFleet(12, [10, 8, 0, 5, 3], [2, 4, 1, 1, 3])); // 3
+console.log(carFleet(10, [3], [3])); // 1
+console.log(carFleet(100, [0, 2, 4], [4, 2, 1])); // 1
