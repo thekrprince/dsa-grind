3fefc941d8c55c21b1f285c5f13113c5eec64602
thekrprince <princekr700@gmail.com>
2025-09-16 22:58:13 +0530

    middle of the linked list

--- LeetCode/876. Middle-of-the-linked-list.js
+++ LeetCode/876. Middle-of-the-linked-list.js
@@ -1,1 +1,22 @@
 // 876. Middle of the Linked List
+
+import { arrayToList } from "./arrayToLinkedList";
+
+// LC 876. Middle of the Linked List
+function middleNode(head) {
+    let slow = head;
+    let fast = head;
+
+    while(fast && fast.next) {
+        slow = slow.next;
+        fast = fast.next.next;
+    }
+
+    return slow;
+}
+
+let list1 = arrayToList([1,2,3,4,5]);
+console.log(middleNode(list1));
+
+let list2 = arrayToList([1,2,3,4,5, 6]);
+console.log(middleNode(list2));
