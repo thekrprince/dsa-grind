4f5a841af5d35f374014afa67585acfcc68679f5
thekrprince <princekr700@gmail.com>
2026-04-11 16:55:01 +0530

    fibonacci

---
+++ NamDSA/4. Recursion/7. fibonacci.js
@@ -0,0 +1,12 @@
+// Fibonacci Series - 0, 1, 1, 2, 3, 5, 8, 13, 21, 34,...........n
+
+function fib(n) {
+    if (n <= 1) {
+        return n;
+    }
+
+    return fib(n - 1) + fib(n - 2);
+}
+
+console.log(fib(6));
+console.log(fib(8));
