84a8b0f2584cb53c61bb047ff27850d07fdd29e2
thekrprince <princekr700@gmail.com>
2026-05-16 00:31:05 +0530

    search 2D matrix

--- NeetCode-250/5. Binary Search/5. searchA2DMatrix.js
+++ NeetCode-250/5. Binary Search/5. searchA2DMatrix.js
@@ -1,1 +1,23 @@
 // LC 74 - Search a 2D Matrix
+
+function searchMatrix(matrix, target) {
+    let i = 0;
+    let j = matrix[0].length - 1;
+
+    while (i <= matrix.length - 1 && j >= 0) {
+        if (matrix[i][j] === target) {
+            return true;
+        } else if (matrix[i][j] < target) {
+            i++;
+        } else {
+            j--;
+        }
+    }
+
+    return false;
+}
+
+let matrix = [[1, 3, 5, 7], [10, 11, 16, 20], [23, 30, 34, 60]];
+
+console.log(searchMatrix(matrix, 3));
+console.log(searchMatrix(matrix, 18));
