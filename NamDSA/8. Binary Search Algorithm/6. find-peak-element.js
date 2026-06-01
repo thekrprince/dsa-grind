fa15c4dab2a984e610f4c5383bd570ec9197d4b5
thekrprince <princekr700@gmail.com>
2026-06-01 22:25:24 +0530

    find peak element

--- NamDSA/8. Binary Search Algorithm/6. find-peak-element.js
+++ NamDSA/8. Binary Search Algorithm/6. find-peak-element.js
@@ -1,1 +1,22 @@
 // LC 162. Find Peak Element
+// A peak element is an element that is strictly greater than its neighbors.
+
+function findPeakElement(nums) {
+    let left = 0;
+    let right = nums.length - 1;
+
+    while (left < right) {
+        const mid = left + Math.floor((right - left) / 2);
+
+        if (nums[mid] > nums[mid + 1]) {
+            right = mid;
+        } else {
+            left = mid + 1;
+        }
+    }
+
+    return right;
+}
+
+console.log(findPeakElement([1, 2, 3, 1])); // 2
+console.log(findPeakElement([1, 2, 1, 3, 5, 6, 4])); // 5
