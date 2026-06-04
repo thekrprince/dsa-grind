d3ac232cd1b8e8d6dea2276e0fd16fa55d7202fe
thekrprince <princekr700@gmail.com>
2026-06-04 23:06:54 +0530

    incomplete sol

--- NamDSA/8. Binary Search Algorithm/8. find-first-and-last-position-in-sortedarray.js
+++ NamDSA/8. Binary Search Algorithm/8. find-first-and-last-position-in-sortedarray.js
@@ -1,1 +1,39 @@
 // LC 34 - FInd First and Last Position of Element in Sorted Array
+
+function findFirstAndLastPosition(arr, target) {
+    let left = 0;
+    let right = arr.length - 1;
+    let ans = [-1, -1];
+
+    while (left < right) {
+        const mid = left + Math.floor((right - left) / 2);
+
+        if (arr[mid] > target) {
+            right = mid - 1;
+        } else {
+            left = mid;
+        }
+    }
+    ans[0] = left;
+
+    left = 0;
+    right = arr.length - 1;
+
+    // while (left < right) {
+    //     const mid = left + Math.ceil((right - left) / 2);
+
+    //     if (arr[mid] < target) {
+    //         left = mid + 1;
+    //     } else {
+    //         right = mid;
+    //     }
+    // }
+
+    // ans[1] = left;
+    console.log(ans);
+    return ans;
+}
+
+console.log(findFirstAndLastPosition([2, 4, 4, 6, 6, 6, 6, 7, 10, 10], 6));
+console.log(findFirstAndLastPosition([2, 4, 4, 5, 6, 6, 6, 7, 10, 10], 10));
+console.log(findFirstAndLastPosition([2, 4, 4, 6, 6, 6, 6, 7, 10, 10], 2));
