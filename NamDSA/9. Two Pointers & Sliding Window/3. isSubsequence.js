d1184cef18eb5c0d57ea74a5dbbbdb9cf0fea358
thekrprince <princekr700@gmail.com>
2026-07-12 16:54:46 +0530

    isSub

--- NamDSA/9. Two Pointers & Sliding Window/3. isSubsequence.js
+++ NamDSA/9. Two Pointers & Sliding Window/3. isSubsequence.js
@@ -1,1 +1,17 @@
 // LC 392 - Is Subsequence
+
+function isSubsequence(s, t) {
+    let i = j = 0;
+
+    while (j < t.length) {
+        if (s[i] === t[j]) {
+            i++;
+        }
+        j++;
+    }
+
+    return i === s.length;
+}
+
+console.log(isSubsequence("abc", "aghbmnce "));
+console.log(isSubsequence("ade", "aghbmnce "));
